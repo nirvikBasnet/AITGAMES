82c039d194283d5ad963aa91551f67d6223c458b
Roy <roy.felix1609@gmail.com>
2019-10-07 13:08:48 +1100

    online database

--- classes/Database.php
+++ classes/Database.php
@@ -1,11 +1,25 @@
 <?php
 namespace aitSydney;
-
 class Database{
+    private $user;
+    private $password;
+    private $host;
+    private $db;
     protected $connection;
-    public function __construct(){
-        $this -> connection = mysqli_connect('localhost','root','','aitgames');
+    protected function __construct(){
+        $this -> getConfig();
+        $this -> connection = mysqli_connect(
+            $this -> host,
+            $this -> user,
+            $this -> password,
+            $this -> db
+        );
+    }
+    private function getConfig(){
+        $this -> user = getenv('dbuser');
+        $this -> password = getenv('dbpass');
+        $this -> host = getenv('dbhost');
+        $this -> db = getenv('dbname');
     }
 }
 ?>
-
